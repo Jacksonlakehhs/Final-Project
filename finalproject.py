33ee24d8359fddf44e5cf70e9f543a2aa707ca47
Jacksonlakehhs <42844689+Jacksonlakehhs@users.noreply.github.com>
2018-12-21 23:37:45 -0500

    Updated from Brython Server: 12/21/2018 11:37:45 PM

--- finalproject.py
+++ finalproject.py
@@ -60,7 +60,7 @@ class Pong(App):
         bg_asset = ImageAsset("images/istockphoto-114445289-612x612.jpg")
         bg = Sprite(bg_asset, (-100, -115))
         bg.scale = 2
-        self.paddle1 = Paddle((100, 300))
+        self.paddle = Paddle((100, 300))
         paddle.scale = 0.2
     
 
