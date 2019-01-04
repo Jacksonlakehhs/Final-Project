558721befec6f4507b0479c42014473d703e2e01
Jacksonlakehhs <42844689+Jacksonlakehhs@users.noreply.github.com>
2019-01-04 14:46:42 -0500

    Updated from Brython Server: 1/4/2019 2:46:41 PM

--- finalproject.py
+++ finalproject.py
@@ -58,7 +58,7 @@ class Ball(Sprite):
             self.visible = False
 
 class Paddle1(Sprite):
-    paddle1 = RectangleAsset(50, 500, thinline, red)
+    paddle1 = RectangleAsset(50, 420, thinline, red)
     
     def __init__(self, position):
         super().__init__(Paddle1.paddle1, position)
@@ -95,7 +95,7 @@ class Paddle1(Sprite):
     
 
 class Paddle2(Sprite):
-    paddle2 = RectangleAsset(50, 500, thinline, red)
+    paddle2 = RectangleAsset(50, 420, thinline, red)
     
     def __init__(self, position):
         super().__init__(Paddle2.paddle2, position)
